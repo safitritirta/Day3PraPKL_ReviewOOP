8389083b4ebd79d0830de99fa35bfc0106bc5884
safitritirta <stirtalestari@gmail.com>
2022-07-07 15:56:55 +0700

    ganti tambah data

--- Guru/create.php
+++ Guru/create.php
@@ -38,7 +38,7 @@
         <div class="container">
         <div class="card">
             <div class="card-header">
-                Tambah Dataa
+                Tambah Data
             </div>
             <div class="card-body">
                 
